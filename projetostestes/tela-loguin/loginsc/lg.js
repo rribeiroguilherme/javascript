3f21a8332a596bc1488fb9b990f93ce5a78b96e9
Guilherme <rribeirooguilherme@gmail.com>
2025-02-01 12:28:02 -0300

    a

--- projetostestes/tela-loguin/loginsc/lg.js
+++ projetostestes/tela-loguin/loginsc/lg.js
@@ -1,3 +1,12 @@
 function login() {
-    document.write('LETICIA NAO TA PRONTO KKKKKKKKKKKK')
+    
+    var us = document.getElementById('em')
+    var s = document.getElementById('se')
+    var u = us.value
+    var ss = s.value
+    if (us.value.length == 0 || s.value.length == 0) {
+        window.alert('[ERRO] Preencha os dados e tente novamente!')
+    } else {
+        window.document.write('LETICIA NAO TA PRONTO KKKKKKKKKKKK')
+    }
 }
